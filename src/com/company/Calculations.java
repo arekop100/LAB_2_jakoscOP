70f542eaa946cb64c6782b5c5403d5adace28f14
arekop100 <arekop100@gmail.com>
2020-03-13 15:49:23 +0100

    JAVA

---
+++ src/com/company/Calculations.java
@@ -0,0 +1,42 @@
+package com.company;
+
+public class Calculations {
+
+
+    public static Point2D positionGeometricCenter(Point2D[ ] points){
+        double temp_x = 0;
+        double temp_y = 0;
+
+        for(Point2D point2D: points){
+            temp_x += point2D.x;
+            temp_y += point2D.y;
+        }
+
+        temp_x = temp_x / points.length;
+        temp_y = temp_y / points.length;
+
+        return new Point2D(temp_x, temp_y);
+
+    }
+
+
+    public static Point2D positionCenterOfMass(MaterialPoint2D[ ] materialPoint){
+        double temp_x=0;
+        double temp_y=0;
+        double temp_mass=0;
+
+        for(MaterialPoint2D materialPoint2D: materialPoint) {
+            temp_x += materialPoint2D.getX() * materialPoint2D.getMass();
+            temp_y += materialPoint2D.getY() * materialPoint2D.getMass();
+            temp_mass += materialPoint2D.getMass();
+        }
+
+        temp_x = temp_x/temp_mass;
+        temp_y = temp_y/temp_mass;
+
+        return new MaterialPoint2D(temp_x, temp_y ,temp_mass);
+
+    }
+
+
+}
---
+++ src/com/company/MaterialPoint2D.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class MaterialPoint2D extends com.company.Point2D {
+
+    private double mass = 0;
+
+    @Override
+    public String toString() {
+        return "MaterialPoint2D{" +
+                "x=" + x +
+                ", y=" + y +
+                ", mass=" + mass +
+                '}';
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public MaterialPoint2D (double x, double y, double mass){
+        super(x,y);
+        this.mass=mass;
+
+    }
+}
+
